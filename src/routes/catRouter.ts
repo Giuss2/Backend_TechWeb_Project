b78624534b9802454f8dde8978415bce6c075681
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2025-10-21 13:53:37 +0200

    Router for cats (working on it)

---
+++ src/routes/catRouter.ts
@@ -0,0 +1,32 @@
+import express, { type NextFunction, type Request, type Response } from "express";
+import { CatController } from "../controllers/CatController.js";
+import {ensureUsersModifyOnlyOwnCats} from "../middleware/authorization.js";
+
+export const catRouter = new express.Router();
+
+catRouter.get("/cats", (req: Request, res: Response, next: NextFunction) => {
+  CatController.getCatsForCurrentUser(req).then(catItems => {
+    res.json(catItems)
+  }).catch(err => {
+    next(err);
+  });
+});
+
+catRouter.post("/cats", (req: Request, res: Response, next: NextFunction) => {
+  CatController.saveCat(req).then( result => {
+    res.json(result);
+  }).catch(err => {
+    next(err);
+  });
+});
+
+catRouter.get("/cats/:id", ensureUsersModifyOnlyOwnCats, (req: Request, res: Response, next: NextFunction) => {
+  CatController.findById(req).then( (item) => {
+    if(item)
+      res.json(item);
+    else 
+      next({status: 404, message: "cat not found"});
+  }).catch( err => {
+    next(err);
+  })
+});
